6fd9f89efbe061a681509bc4a9eeabf5032ed66a
elcaneyvazli007 <elcaneyvazli77@gmail.com>
2023-03-05 12:26:12 +0400

    sjsjjs

--- src/component/modal.jsx
+++ src/component/modal.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import {AiOutlineCloseCircle} from 'react-icons/ai'
+import React from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
-const modal = () => {
+const modal = ({ modal, setModal }) => {
   return (
     <div className="fixed h-screen w-screen top-0 right-0 bottom-0 grid place-content-center">
-        <div className="relative bg-white w-[60rem] h-[20rem] rounded-[1.5rem] flex flex-col px-[2rem] py-[3rem]">
-            <div className="relative flex z-50 flex-row items-center justify-between w-full">
-                <h1 className="text-[1.8rem] text-primary font-bold">Rabitə Xətti</h1>
-            </div>
+      <div className="relative bg-white w-[60rem] h-[20rem] rounded-[1.5rem] flex flex-col px-[2rem] py-[3rem]">
+        <div className="relative flex z-50 flex-row items-center justify-between w-full">
+          <h1 className="text-[1.8rem] text-primary font-bold">Rabitə Xətti</h1>
+          <AiOutlineCloseCircle
+            className="text-[3rem] cursor-pointer"
+            onClick={() => setModal(false)}
+          />
         </div>
-        <AiOutlineCloseCircle className='text-[3rem]' onClick={() => setModal(false)}/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default modal
+export default modal;
